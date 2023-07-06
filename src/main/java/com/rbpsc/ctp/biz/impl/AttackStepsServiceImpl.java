d2d3f9e4c3542c3895eafb3eb949db115879e52e
Zhe Xu <zhexu@umass.edu>
2023-07-05 23:11:04 -0400

    [MODIFY] Start to adjust logics of AttackStepsService

--- src/main/java/com/rbpsc/ctp/biz/impl/AttackStepsServiceImpl.java
+++ src/main/java/com/rbpsc/ctp/biz/impl/AttackStepsServiceImpl.java
@@ -1,12 +1,17 @@
 package com.rbpsc.ctp.biz.impl;
 
 import com.rbpsc.ctp.api.entities.dto.response.DrugLifeCycleResponse;
+import com.rbpsc.ctp.api.entities.factories.DataEntityFactory;
+import com.rbpsc.ctp.api.entities.supplychain.drug.DrugInfo;
+import com.rbpsc.ctp.api.entities.supplychain.drug.DrugLifeCycle;
+import com.rbpsc.ctp.api.entities.supplychain.operations.Receipt;
 import com.rbpsc.ctp.api.entities.supplychain.operations.attack.AttackAvailability;
 import com.rbpsc.ctp.api.entities.supplychain.operations.attack.AttackConfidentiality;
 import com.rbpsc.ctp.api.entities.supplychain.operations.attack.AttackIntegrity;
 import com.rbpsc.ctp.biz.service.AttackStepsService;
 import com.rbpsc.ctp.common.utiles.WebClientUtil;
 import com.rbpsc.ctp.repository.service.AttackConfidentialityRepository;
+import com.rbpsc.ctp.repository.service.DrugLifeCycleReceiptRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -17,12 +22,11 @@ public class AttackStepsServiceImpl implements AttackStepsService {
     final
     WebClientUtil webClientUtil;
 
-    final
-    AttackConfidentialityRepository attackConfidentialityRepository;
+    final DrugLifeCycleReceiptRepository drugLifeCycleReceiptRepository;
 
-    public AttackStepsServiceImpl(WebClientUtil webClientUtil, AttackConfidentialityRepository attackConfidentialityRepository) {
+    public AttackStepsServiceImpl(WebClientUtil webClientUtil, AttackConfidentialityRepository attackConfidentialityRepository, DrugLifeCycleReceiptRepository drugLifeCycleReceiptRepository) {
         this.webClientUtil = webClientUtil;
-        this.attackConfidentialityRepository = attackConfidentialityRepository;
+        this.drugLifeCycleReceiptRepository = drugLifeCycleReceiptRepository;
     }
 
     @Override
@@ -41,19 +45,22 @@ public class AttackStepsServiceImpl implements AttackStepsService {
     }
 
     @Override
-    public boolean attackConfidentiality(AttackConfidentiality attackConfidentiality) {
+    public boolean attackConfidentiality(DrugInfo drug, AttackConfidentiality attackConfidentiality) {
 
         // TODO: add "receipt queue"
         // TODO: put fake receipt back in the receipt queue
         // TODO: replace ExampleMsg here
 
-        attackConfidentialityRepository.insertAttack(attackConfidentiality);
+        DrugLifeCycle<Receipt> receiptDrugLifeCycle = drugLifeCycleReceiptRepository.selectDrugLifeCycleReceiptById(drug.getId());
+        if (null == receiptDrugLifeCycle){
+            receiptDrugLifeCycle =  DataEntityFactory.createDrugLifeCycleReceipt(drug);
+        }
 
         return false;
     }
 
     @Override
-    public boolean attackIntegrity(AttackIntegrity attackIntegrity) {
+    public boolean attackIntegrity(DrugInfo drug, AttackIntegrity attackIntegrity) {
         // TODO: Make the selling chain stop here and store "DrugLifeCycle" object
         return false;
     }
--- src/main/java/com/rbpsc/ctp/biz/service/AttackStepsService.java
+++ src/main/java/com/rbpsc/ctp/biz/service/AttackStepsService.java
@@ -1,6 +1,7 @@
 package com.rbpsc.ctp.biz.service;
 
 
+import com.rbpsc.ctp.api.entities.supplychain.drug.DrugInfo;
 import com.rbpsc.ctp.api.entities.supplychain.operations.attack.AttackAvailability;
 import com.rbpsc.ctp.api.entities.supplychain.operations.attack.AttackConfidentiality;
 import com.rbpsc.ctp.api.entities.supplychain.operations.attack.AttackIntegrity;
@@ -8,7 +9,7 @@ import com.rbpsc.ctp.api.entities.supplychain.operations.attack.AttackIntegrity;
 public interface AttackStepsService {
     public boolean attackAvailability(AttackAvailability attackAvailability);
 
-    public boolean attackConfidentiality(AttackConfidentiality attackConfidentiality);
+    public boolean attackConfidentiality(DrugInfo drug, AttackConfidentiality attackConfidentiality);
 
-    public boolean attackIntegrity(AttackIntegrity attackIntegrity);
+    public boolean attackIntegrity(DrugInfo drug, AttackIntegrity attackIntegrity);
 }
