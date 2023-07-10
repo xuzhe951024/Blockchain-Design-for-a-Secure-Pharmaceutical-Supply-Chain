a6058d39cf9838dddf8c15352d1c72e515ad21d2
xuzhe951024 <xuzhe951024@live.com>
2023-07-10 19:43:56 -0400

    [ADD] Add echo debug commands to start and stop shell script in testAuto

--- testAuto/start.sh
+++ testAuto/start.sh
@@ -1,2 +1,4 @@
+echo "Creating network..."
 docker network create psc-testnet
+echo "Starting docker-compose..."
 docker-compose -f basic-docker-env.yml up -d
--- testAuto/stopAndClean.sh
+++ testAuto/stopAndClean.sh
@@ -1,5 +1,10 @@
+echo "Starting script..."
 docker-compose -f basic-docker-env.yml down
+echo "Executing next command..."
 docker container stop $(docker container ls --filter network=psc-testnet -q)
+echo "Executing next command..."
 docker container rm $(docker container ls -a --filter network=psc-testnet -q)
+echo "Executing next command..."
 docker rmi  $(docker images --filter "label=group=psc" --format "{{.Repository}}")
+echo "Executing next command..."
 docker network rm psc-testnet
